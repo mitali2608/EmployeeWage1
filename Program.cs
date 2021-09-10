4ba507f8589a553dea2f09ad9ffdd51b3a25def6
mitali2608 <mitalibawankule21@gmail.com>
2021-09-10 12:31:52 +0530

    Mitali : Added this file to compile Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UC1_EmployeeAttendance.GetAttendance();
+            UC2_EmpWage.EmpWage();
+            UC3_PartTimeEmpWage.PartTimeEmpWage();
+            UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            UC5_CalculateWagesFor20DaysInMonth.CalculateWagesFor20DaysInMonth();
+            UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+            UC7_ComputeEmpWageUsingClassMethod.ComputeEmpWage();
+            UC8_ComputeEmpWageForMultipleCompanies.ComputeEmpWage("xyz", 20, 22, 35);
+            Console.ReadKey();
+        }
+    }
+}
